b1b01b16bb3a374008d08d1a285aa2a9a879eae8
Kaique <sacchikaique@gmail.com>
2020-06-30 00:12:00 -0300

    Added README. Added more videos
    Change-Id: I6c319497113e8a95ee7ff5863acdc729d07f040d

--- video-player/src/pages/Home/index.tsx
+++ video-player/src/pages/Home/index.tsx
@@ -32,6 +32,36 @@ export default function Home() {
       title: 'Simulação de Fluidos - 16m/s',
       description:
         'Vídeo gerado utilizando a engine de simulação de fluidos do Blender. A velocidade do fluxo de água é de 16m/s. Nesse cenário, o balanço do barco é muito mais violento por conta da turbulência gerada pela água.'
+    },
+    {
+      url: 'https://youtu.be/meMMRPUOPtM',
+      title: 'Simulação por Textura - Vista do pier',
+      description: 'Vídeo gerado utilizando texturas dinâmicas para simular a física de fluidos.'
+    },
+    {
+      url: 'https://youtu.be/iL0EpZF8TX8',
+      title: 'Simulação por Textura - Vista em terceira pessoa',
+      description: 'Vídeo gerado utilizando texturas dinâmicas para simular a física de fluidos.'
+    },
+    {
+      url: 'https://youtu.be/MYG7usoM7RI',
+      title: 'Simulação por Textura - Vista frontal',
+      description: 'Vídeo gerado utilizando texturas dinâmicas para simular a física de fluidos.'
+    },
+    {
+      url: 'https://youtu.be/DGOgvsCGiuY',
+      title: 'Simulação por Textura - Vista da grade',
+      description: 'Vídeo gerado utilizando texturas dinâmicas para simular a física de fluidos.'
+    },
+    {
+      url: 'https://youtu.be/-0hbm4LT8rw',
+      title: 'Simulação por Textura - Vista do ambiente',
+      description: 'Vídeo gerado utilizando texturas dinâmicas para simular a física de fluidos.'
+    },
+    {
+      url: 'https://youtu.be/eSHGQBzFrgc',
+      title: 'Simulação por Textura - Vista da mesa',
+      description: 'Vídeo gerado utilizando texturas dinâmicas para simular a física de fluidos.'
     }
   ];
 
